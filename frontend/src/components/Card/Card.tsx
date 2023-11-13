c1bf89e94b6f6a11df1fac9fc0d5deaa24108c5d
AnhTran <letrannhatanh171@gmail.com>
2023-11-13 23:48:49 +0700

    update component

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import { CardProps } from "../type";
 
 const Card: React.FC<CardProps> = (props: CardProps) => {
   return (
-    <div className="relative dark:bg-bgModalDark flex flex-col items-center justify-center rounded-lg shadow-xl py-10 px-6 mb-4">
+    <div className="relative dark:bg-bgModalDark flex items-center justify-center rounded-lg shadow-xl py-10 px-6 mb-4">
       {props.children}
     </div>
   );
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -62,6 +62,7 @@ const Register: React.FC<RegisterProps> = ({
           </div>
           <div className="mb-6 flex justify-center flex-col">
             <input
+              type="password"
               {...register("password", { required: true })}
               name="password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-blue-500 focus:border-blue-500 block w-96 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
